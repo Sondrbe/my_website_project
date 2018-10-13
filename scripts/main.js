2809b2f09c27b2aee9fe7b9d7044286cd917b695
Sondrbe <sondrebergo@gmail.com>
2018-10-13 20:39:35 +0200

    My running website!

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+var myImage = document.querySelector('img');
+
+
+function bestFunc() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox.png') {
+      myImage.setAttribute ('src','images/itro.jpg');
+    } else {
+      myImage.setAttribute ('src','images/firefox.png');
+    }
+}
+
+
+myImage.onclick = bestFunc
+
+
+function myFunc(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
+
+function add(num1, num2){
+    var result = num1 + num2;
+    return result;
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
